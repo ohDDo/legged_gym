45561ddb5ae549663878ca122fe133401c4b6ef6
ohDDo <dasdas12002@gmail.com>
2025-06-13 15:14:33 +0900

    set: for 3070ti

--- legged_gym/envs/anymal_c/mixed_terrains/anymal_c_rough_config.py
+++ legged_gym/envs/anymal_c/mixed_terrains/anymal_c_rough_config.py
@@ -32,11 +32,13 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 class AnymalCRoughCfg( LeggedRobotCfg ):
     class env( LeggedRobotCfg.env ):
-        num_envs = 4096
+        num_envs = 2048
         num_actions = 12
 
     class terrain( LeggedRobotCfg.terrain ):
         mesh_type = 'trimesh'
+        num_rows= 8 # number of terrain rows (levels)
+        num_cols = 10 # number of terrain cols (types)
 
     class init_state( LeggedRobotCfg.init_state ):
         pos = [0.0, 0.0, 0.6] # x,y,z [m]
